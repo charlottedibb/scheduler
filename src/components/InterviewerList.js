ef3e5fe00fb08e308a4ae2c4198fd3916483e37d
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-28 16:56:50 -0700

    Update component to render data dynamically

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,11 +1,29 @@
 import React from "react";
 import "components/InterviewerList.scss";
+import InterviewerListItem from "components/InterviewerListItem";
+
+// InterviewerList takes in the following props:
+// interviewers:array - an array of objects containing the information of each interviewer
+// interviewer:number - the id of an interviewer
+// setInterviewer:function - a function that accepts an interviewer id
 
 export default function InterviewerList(props) {
+  const interviewers = props.interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem
+      key={interviewer.id}
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected={interviewer.id === props.interviewer}
+      setInterviewer={props.setInterviewer}
+      />
+      )
+  })
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{interviewers}</ul>
     </section>
   );
 }
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,17 +1,29 @@
 import React from "react";
 import "components/InterviewerListItem.scss";
+const classnames = require('classnames');
 
+// InterviewerListItem takes in the following props:
+// id:number - the id of the interviewer
+// name:string - the name of the interviewer
+// avatar:url - a url to an image of the interviewer
+// selected:boolean - to determine if an interview is selected or not
+// setInterviewer:function - sets the interviewer upon selection
 
 export default function InterviewerListItem(props) {
+  const interviewerClass = classnames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  })
   return (
-    <li className="interviewers__item">
+    <li
+      className={interviewerClass}
+      onClick={() => props.setInterviewer(props.name)}
+      id={props.id}>
       <img
         className="interviewers__item-image"
-        src="https://i.imgur.com/LpaY82x.png"
-        alt="Sylvia Palmer"
+        src={props.avatar}
+        alt={props.name}
       />
-      Sylvia Palmer
-</li>
+      {props.name}
+    </li>
   );
 }
-
