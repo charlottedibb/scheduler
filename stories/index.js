157b969ea9881dfc714c0104727578ebb23c9b79
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-28 19:00:50 -0700

    Create story for appointment Show component

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 // button
 storiesOf("Button", module)
@@ -147,5 +148,12 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with time", () => <Appointment time="12pm" />)
-  .add("Header", () => <Header time="12pm"/>)
-  .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => (
+    <Show 
+    student="Lydia Miller-Jones"
+    interviewer={interviewer}
+    onEdit={action("onEdit")}
+    onDelete={action("onDelete")}
+    />))
