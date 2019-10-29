780f090d809bfe2ca4eb1cb52b5484c1a23e78f4
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-28 20:35:36 -0700

    Create Form edit and Form create stories

--- stories/index.js
+++ stories/index.js
@@ -19,6 +19,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 // button
 storiesOf("Button", module)
@@ -172,5 +173,21 @@ storiesOf("Appointment", module)
     <Error
       message="Could not delete appointment."
       onClose={action("onClose")}
+    />
+  ))
+  .add("Form edit", () => (
+    <Form
+      name="Student McStudentFace"
+      interviewers={interviewers}
+      interviewer={1}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Form create", () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
     />
   ))
