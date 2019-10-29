b1484e20c3b7509776f533491130f94837a56629
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-28 19:34:54 -0700

    Create story for appointment Status component

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 // button
 storiesOf("Button", module)
@@ -165,4 +166,5 @@ storiesOf("Appointment", module)
       onCancel={action("onCancel")}
     />
   ))
+  .add("Status", () => <Status message="Deleting" />)
 
