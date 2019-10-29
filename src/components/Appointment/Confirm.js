d19eec962ca0f9088cd665e82a4ee960d4e3e42f
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-28 19:27:35 -0700

    Update Confirm to take in props

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -2,13 +2,18 @@ import React from "react";
 
 import Button from "components/Button.js";
 
+// The Confirm component accepts the following props:
+// message:String eg. "Delete the appointment?"
+// onConfirm:Function to be called when the user clicks the Confirm button
+// onCancel:Function to be called when the user clicks the Cancel button
+
 export default function Confirm(props) {
   return (
     <main className="appointment__card appointment__card--confirm">
-      <h1 className="text--semi-bold">Delete the appointment?</h1>
+      <h1 className="text--semi-bold">{props.message}</h1>
       <section className="appointment__actions">
-        <Button danger>Cancel</Button>
-        <Button danger>Confirm</Button>
+        <Button danger onClick={props.onCancel}>Cancel</Button>
+        <Button danger onClick={props.onConfirm}>Confirm</Button>
       </section>
     </main>
   )
