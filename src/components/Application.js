b9a52ce8dbcea78ed1520dbdab1129cbf5913fc5
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-30 13:32:19 -0700

    Display correct interviewer name for each of our appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,15 +2,16 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 import "components/Application.scss";
 
 export default function Application(props) {
- 
+
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const setDay = day => setState({ ...state, day });
@@ -18,20 +19,30 @@ export default function Application(props) {
   useEffect(() => {
     Promise.all([
       axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments")
+      axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers")
     ]).then((response) => {
-        setState(prev => ({
-          ...prev,
-          days: response[0].data, //array of days
-          appointments: response[1].data //appointment objects
-        }));
-      })
+      setState(prev => ({
+        ...prev,
+        days: response[0].data, //array of days
+        appointments: response[1].data, //appointment objects
+        interviewers: response[2].data //interviewer objects
+      }));
+    })
   }, [])
 
   const appointments = getAppointmentsForDay(state, state.day);
 
   const schedule = appointments.map((appointment) => {
-    return (<Appointment key={appointment.id} {...appointment} />)
+    const interview = getInterview(state, appointment.interview);
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+      />
+    );
   })
 
   return (
