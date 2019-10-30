938d7315407a2426acab5793a3539ac1389b6896
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-30 16:01:19 -0700

    Create replace argument for transition function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -7,14 +7,18 @@ export default function useVisualMode(initial) {
   // initializes history as an array with the intial mode pased in 
   const [history, setHistory] = useState([initial]);
 
-  function transition(mode) {
+  function transition(mode, replace) {
     setMode(mode); //updates mode with new mode value
-    setHistory([...history, mode]); //adds new mode to history
+    if (replace) {
+      setHistory([...history.slice(0, history.length - 1), mode]); //adds new mode to history
+    } else {
+      setHistory([...history, mode]); //adds new mode to history
+    }
   }
 
   function back() {
     if (history.length > 1) {
-      setMode(history[history.length - 2]); // sets mode to prev item in history array
+      setMode(history[history.length - 2]); // sets mode to 2nd last item in history array
       setHistory(history.slice(0, history.length - 1)); //removes last mode from history
     }
   }
