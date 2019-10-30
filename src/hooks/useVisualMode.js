37992658fbd5675902ee8c73509a71223c70f1d0
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-30 14:06:43 -0700

    Create transition function within useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,7 +2,12 @@ import React, { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial); //set the mode state with the intital mode provided
+  function transition(newMode) {
+    setMode(newMode);
+  }
+  
   return {
-    mode
+    mode,
+    transition
   }
 }
