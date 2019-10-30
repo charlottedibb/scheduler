e63a478912a30145c7170117babd02c4fc263e0e
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-29 19:02:17 -0700

    Update Application component to use state instead of hard-coded data

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,57 +2,11 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
+import { getAppointmentsForDay } from "helpers/selectors";
 import "components/Application.scss";
 
-//test appointment data
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "3pm",
-    interview: {
-      student: "Ya girl",
-      interviewer: {
-        id: 5,
-        name: "Sven Jones",
-        avatar: "https://i.imgur.com/twYrpay.jpg",
-      }
-    }
-  },
-  {
-    id: 12,
-    time: "6pm",
-    interview: {
-      student: "Gang gang",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 84,
-    time: "7pm",
-    interview: {
-      student: "Y E E T",
-      interviewer: {
-        id: 3,
-        name: "Mildred Nazir",
-        avatar: "https://i.imgur.com/T2WwVfS.png",
-      }
-    }
-  }
-];
-
 export default function Application(props) {
-  const schedule = appointments.map((appointment) => {
-    return (<Appointment key={appointment.id} {...appointment} />)
-  })
-
+ 
   const [state, setState] = useState({
     day: "Monday",
     days: [],
@@ -74,7 +28,11 @@ export default function Application(props) {
       })
   }, [])
 
+  const appointments = getAppointmentsForDay(state, state.day);
 
+  const schedule = appointments.map((appointment) => {
+    return (<Appointment key={appointment.id} {...appointment} />)
+  })
 
   return (
     <main className="layout">
